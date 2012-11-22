7a46e166c7717eefc3f8422d2bf50817e6143e57
franck <carnet.franck.paul@gmail.com>
2012-11-22 17:12:48 +0100

    Now uses the standard message way

--- autosave.js
+++ autosave.js
@@ -1,39 +1,33 @@
-if (!window.console || !console.firebug) {
-	var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml", "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
-	window.console = {};
-	for (var i = 0; i < names.length; ++i) window.console[names[i]] = function() {};
-}
-
 $(document).ready(function(){
 	if ($('#id').length) {
-		/* grouik ! trouver une autre solution, cause un bug dans mon firefox, 
-			le formulaire est tout le temps vu comme dirty, même si aucun contenu n'a été modifié
-		*/
 		$('#entry-form').append('<input type="hidden" id="save" value="save" name="save" />');
 		$('#entry-form').autosave({
 			/** Timer durations **/
 			interval: 	30000,
 			monitor: 	3000,
 			/** Callbacks **/
-			setup: 		function(e,o) {
-				console.log("Setting up autosaver...");
-			},
-			record: 	function(e,o) {
-				console.log("Recording form's state for the autosaver...");
-			},
 			save: 		function(e,o) {
 				d = new Date();
-				$('#footer p').text('Autosave : dernier enregistrement le '+d.toLocaleString());					
-				console.log("Autosaver is now saving...")
-			},
-			shutdown: 	function(e,o) {
-				console.log("Shutting down autosaver...");
-			},
-			dirty: 		function(e,o) {
-				console.log("Autosaver detected a dirty form...");
+				t = 'Autosave : dernier enregistrement à '+d.toLocaleTimeString();
+				if ($('#content p.message').length > 0) {
+					$('#content p.message').text(t);
+				} else {
+					var msg = $(document.createElement('p'));
+					msg.addClass('message');
+					msg.text(t);
+					$('#content').prepend(msg);
+				}
 			}
 		});
 	} else {
-		$('#footer p').text('Autosave désactivé ! y faut enregistrer une première fois le post pour que ça roule');
+		t = 'Autosave : en attente d\'un premier enregistrement';
+		if ($('#content p.message').length > 0) {
+			$('#content p.message').text(t);
+		} else {
+			var msg = $(document.createElement('p'));
+			msg.addClass('message');
+			msg.text(t);
+			$('#content').prepend(msg);
+		}
 	}
 });
