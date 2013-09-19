457da79e9fa176ea82a48e1379eafdc3ff481a77
franck <carnet.franck.paul@gmail.com>
2013-09-19 13:32:06 +0200

    various (next release will be 0.5)

--- _admin.php
+++ _admin.php
@@ -1,14 +1,15 @@
 <?php
 # -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of autosave, a plugin for Dotclear 2.
 #
-# This file is part of Dotclear 2.
+# Copyright (c) biou, Franck Paul and contributors
+# carnet.franck.paul@gmail.com
 #
-# Copyright (c) 2003-2012 Olivier Meunier and contributors
 # Licensed under the GPL version 2.0 license.
-# See LICENSE file or
+# A copy of this license is available in LICENSE file or at
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-#
 # -- END LICENSE BLOCK ------------------------------------
+
 if (!defined('DC_CONTEXT_ADMIN')) { return; }
 
 $core->addBehavior('adminPostHeaders',array('autosaveBehaviors','jsLoad'));
--- _define.php
+++ _define.php
@@ -1,21 +1,22 @@
 <?php
 # -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of autosave, a plugin for Dotclear 2.
 #
-# This file is part of Dotclear 2.
+# Copyright (c) biou, Franck Paul and contributors
+# carnet.franck.paul@gmail.com
 #
-# Copyright (c) 2003-2012 Olivier Meunier and contributors
 # Licensed under the GPL version 2.0 license.
-# See LICENSE file or
+# A copy of this license is available in LICENSE file or at
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-#
 # -- END LICENSE BLOCK ------------------------------------
+
 if (!defined('DC_RC_PATH')) { return; }
 
 $this->registerModule(
 	/* Name */			"Autosave",
 	/* Description*/		"Autosave entry during edition",
 	/* Author */			"Alain Vagner, Franck Paul",
-	/* Version */			'0.4.2',
+	/* Version */			'0.5',
 	/* Permissions */		'usage,contentadmin',
 	/* Priority */			50
 );
