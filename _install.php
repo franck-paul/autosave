410a77a2b971b7eb054380d15a6adcb0db5929d8
franck <carnet.franck.paul@gmail.com>
2022-12-26 17:47:04 +0100

    New install

--- _install.php
+++ _install.php
@@ -14,21 +14,15 @@ if (!defined('DC_CONTEXT_ADMIN')) {
     return;
 }
 
-$new_version = dcCore::app()->plugins->moduleInfo('autosave', 'version');
-$old_version = dcCore::app()->getVersion('autosave');
-
-if (version_compare((string) $old_version, $new_version, '>=')) {
+if (!dcCore::app()->newVersion(basename(__DIR__), dcCore::app()->plugins->moduleInfo(basename(__DIR__), 'version'))) {
     return;
 }
 
 try {
     // Default user settings
-    dcCore::app()->auth->user_prefs->addWorkspace('interface');
     if (!dcCore::app()->auth->user_prefs->interface->prefExists('autosave')) {
         dcCore::app()->auth->user_prefs->interface->put('autosave', true, 'boolean');
     }
-
-    dcCore::app()->setVersion('autosave', $new_version);
 
     return true;
 } catch (Exception $e) {
