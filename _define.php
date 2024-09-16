394a2952b4c1ed2a3f2d6b551ce924fa4f917e53
franck <carnet.franck.paul@gmail.com>
2024-09-16 16:04:16 +0200

    New release 4.2

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Autosave',
     'Autosave entry during edition',
     'Alain Vagner, Franck Paul',
-    '4.1',
+    '4.2',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
