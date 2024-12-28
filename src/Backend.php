9ad25e238c490c89ce2ce7517d9ad325d7c83910
franck <carnet.franck.paul@gmail.com>
2024-12-28 07:06:39 +0100

    PHP Code review (Rector/PHPStan)

--- src/Backend.php
+++ src/Backend.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @brief autosave, a plugin for Dotclear 2
  *
@@ -22,7 +23,8 @@ class Backend extends Process
     public static function init(): bool
     {
         // dead but useful code, in order to have translations
-        __('Autosave') . __('Autosave entry during edition');
+        __('Autosave');
+        __('Autosave entry during edition');
 
         return self::status(My::checkContext(My::BACKEND));
     }
